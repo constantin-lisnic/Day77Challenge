57a9f04b87094c4cfff2954efd3115a064639e07
Constantin Lisnic <constantin@lisnic.dev>
2024-12-19 17:27:56 +0000

    Initial Commit

---
+++ Day77Challenge/Day77ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day77ChallengeApp.swift
+//  Day77Challenge
+//
+//  Created by Constantin Lisnic on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Day77ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day77ChallengeTests/Day77ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Day77ChallengeTests.swift
+//  Day77ChallengeTests
+//
+//  Created by Constantin Lisnic on 19/12/2024.
+//
+
+import Testing
+@testable import Day77Challenge
+
+struct Day77ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
